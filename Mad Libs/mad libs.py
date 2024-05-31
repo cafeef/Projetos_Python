e5fd64c3ff111b8b00a33c211611fd49ed5c8147
cafeef <cafeef16@gmail.com>
2024-05-30 23:12:28 -0300

    add mad libs

---
+++ Mad Libs/mad libs.py
@@ -0,0 +1,4 @@
+adj = input('Adjetivo: ')
+verbo = input('Verbo: ')
+FamousPerson = input('Pessoa famosa: ')
+print(f'Programar é tão {adj}! Isso me deixa animado o tempo todo porque eu amo {verbo}. Continue tomando água e pule como se você fosse o/a {FamousPerson}')
