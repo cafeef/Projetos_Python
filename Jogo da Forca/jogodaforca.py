83ac41098f84141d0b56a31a810575a10c77124c
cafeef <cafeef16@gmail.com>
2024-06-01 11:57:15 -0300

    add jogo da forca

---
+++ Jogo da Forca/jogodaforca.py
@@ -0,0 +1,5 @@
+def escolha_palavra():
+    lista_palavras = [('maçã', 'fruta vermelha'), ('laranja', 'fruta que também é cor'), ('carro', 'automóvel que todos podem comprar'), ('paralelepípedo', 'forma geométrica espacial'), ('cachorro', 'animal doméstico que não cai em pé'), ('anitta', 'cantora brasileira pedro álvares cabral')]
+    print(lista_palavras['maçã'])
+
+escolha_palavra()
