32bf34d487068d913b98c1c70801f4e036ea636e
fernanda <cafeef16@gmail.com>
2024-06-03 21:15:09 -0300

    att sudoku

--- Sudoku/sudoku.py
+++ Sudoku/sudoku.py
@@ -1,32 +1,50 @@
 from random import randint 
 from random import choice
-Matriz1, Matriz2, Matriz3, Matriz4 = [], [], [], [] * 3
 
+def Criacao_Matriz():
+    sudoku = []
+    linha = 0 
+    while (linha <= 6):
+        lista = []
+        coluna = 0 
+        while (coluna <= 6):
+            elemento = randint(1,9)
+            if elemento in lista:
+                elemento = randint(1,9)                   
+            lista.append(elemento)
+            coluna += 1
+        sudoku.append(lista)
+        linha += 1
+    return sudoku
 
-def atribuicao_linhas():
-    fim = 3
+def Atribuicao_Zeros(sudoku):
+    indices = [0, 1, 2, 3, 4, 5]
+    cont = 0
+    while cont <= 10:
+        indice1 = choice(indices)
+        indice2 = choice(indices)
+        sudoku[indice1][indice2] = 0
+        cont += 1
+
+def Exibicao_Matriz(sudoku_final):
+    i = 0
+    while i <= 5:
+        print(sudoku_final[i])
+        i += 1
+
+def Validade_Matriz(sudoku):
+    i = 0
+    fim = 6
     while fim >= 0:
-        i = []
-        index = 0
-        while index <= (fim - 1):
-            i.append(randint(1, 9))
-            index += 1
+        while i <= (fim - 1):
+            if sudoku[i][fim - 1] in sudoku:
+                sudoku[i][fim - 1] = randint(1, 9)
+            i += 1
         fim -= 1
-    return i
+    return sudoku
 
-def atribuicao_matrizes(linhas, Matriz1, Matriz2, Matriz3, Matriz4):
-    linhas = atribuicao_linhas()
-    Matriz1.append(linhas)
-    linhas = atribuicao_linhas()
-    Matriz2.append(linhas)
-    linhas = atribuicao_linhas()
-    Matriz3.append(linhas)
-    linhas = atribuicao_linhas()
-    Matriz4.append(linhas)
-    Matriz_Total = [Matriz1, Matriz2, Matriz3, Matriz4]    
-    return Matriz_Total
 
-linhas = atribuicao_linhas()
-Matriz_Total = atribuicao_matrizes(linhas, Matriz1, Matriz2, Matriz3, Matriz4)
-
-    
+sudoku = Criacao_Matriz()
+Atribuicao_Zeros(sudoku)
+sudoku_final = Validade_Matriz(sudoku)
+Exibicao_Matriz(sudoku_final)
